cca866333aa724554cb06c4bc786aa5feb17b1a9
vmandalapu1994 <vmandalapu1994@gmail.com>
2024-01-24 20:22:27 +0530

    First commit

---
+++ src/main/java/com/jenkins/mavendemo/MavenDemoApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins.mavendemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenDemoApplication.class, args);
+	}
+
+}
